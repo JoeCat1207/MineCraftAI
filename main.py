9af60d03c6ab141467c0b2c211127a1adc12d381
Matt Botchek <matthew@botchek.com>
2019-09-12 14:47:21 -0500

    Added Main and doAction

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import minerl
 import gym
+import logging
 
+env = None 
 def main():
-    print("Starting AI")
+    print("Started Logging")
+    logging.basicConfig(level=logging.DEBUG)
+    print("Environment Starting")
+    env = gym.make('MineRLNavigateDense-v0')
+    obs = env.reset
+    doAction(False)
+
+def doAction(bool):
+    done = bool
+    while not done:
+        action = env.action_space.sample()
+        obs, reward, done, _ = env.step(action)
+
+if __name__ == "__main__" :
+    main()
+    
 
